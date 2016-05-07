1424ff4e07565f506c5ea90f6f14b7a37b134e23
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2016-05-07 20:14:33 +0200

    Fix description of the Solver interface

--- drencher/src/solver/mod.rs
+++ drencher/src/solver/mod.rs
@@ -14,8 +14,10 @@ pub type Solution = Vec<Color>;
 
 /// Something that can solve our game from an initial board.
 pub trait Solver {
-    /// Given a board, the solver has to return a list of moves that will
-    /// win the game.
+    /// Given a board, the solver has to return a list of moves. Those moves
+    /// either win the game (`Ok(..)`) or don't (`Err(..)`), in which case the
+    /// solver wasn't able to find a winning solution. The `Err` value can
+    /// still contain a solution vector.
     fn solve(&self, b: Board) -> Result<Solution, Solution>;
 
     /// Returns true if the solver already outputs every step of the game. This
